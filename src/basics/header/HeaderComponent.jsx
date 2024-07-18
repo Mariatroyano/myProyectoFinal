9d21d553bb92686153d4f8aaa1330e4d41061f15
mariatroyano <maria2020troyano@gmail.com>
2024-07-18 10:16:01 -0500

    hola

--- src/basics/header/HeaderComponent.jsx
+++ src/basics/header/HeaderComponent.jsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import './HeaderComponent.css';
-import Formulario from '../../collections/app/Formulario'
-import imgen from '../../../public/logo.jpg'
-=======
 import "./HeaderComponent.css";
 import Formulario from "../../collections/app/Formulario";
-import imgen from "C:/Users/Maria Troyano/Desktop/Proyecto-Final/proyecto-final/public/logo.jpg";
->>>>>>> 8523f7a20743e47d66964907a60dbce46d48991f
+import imgen from "../../../public/logo.jpg";
 
 export const HeaderComponent = ({
   notifications,
@@ -19,15 +13,7 @@ export const HeaderComponent = ({
 }) => {
   return (
     <nav className=" headerNav">
-<<<<<<< HEAD
-      <img src={''} alt="" className="" />
-
-      <img className="header__logo" src={imgen} alt="" /> 
-       <h1 className='titulo'>Eleganza </h1>
-      <Formulario/>
-=======
       <img src={""} alt="" className="" />
->>>>>>> 8523f7a20743e47d66964907a60dbce46d48991f
 
       <img className="header__logo" src={imgen} alt="p" />
       <h1 className="titulo">Eleganza </h1>
