8523f7a20743e47d66964907a60dbce46d48991f
Mariatroyano <maria2020troyano@gmail.com>
2024-07-18 09:42:23 -0500

    arreglo de mi proyecto final

--- src/basics/header/HeaderComponent.jsx
+++ src/basics/header/HeaderComponent.jsx
@@ -1,21 +1,23 @@
-import './HeaderComponent.css';
-import Formulario from '../../collections/app/Formulario'
-import imgen from 'C:/Users/Maria Troyano/Desktop/Proyecto-Final/proyecto-final/public/logo.jpg'
+import "./HeaderComponent.css";
+import Formulario from "../../collections/app/Formulario";
+import imgen from "C:/Users/Maria Troyano/Desktop/Proyecto-Final/proyecto-final/public/logo.jpg";
 
-export const HeaderComponent = ({ notifications, username, onLogOut, onSeeDetail, LoGo,img,...props }) => {
+export const HeaderComponent = ({
+  notifications,
+  username,
+  onLogOut,
+  onSeeDetail,
+  LoGo,
+  img,
+  ...props
+}) => {
   return (
     <nav className=" headerNav">
-      <img src={''} alt="" className="" />
+      <img src={""} alt="" className="" />
 
-      <img className="header__logo" src={imgen} alt="p" /> 
-       <h1 className='titulo'>Eleganza </h1>
-      <Formulario/>
-
+      <img className="header__logo" src={imgen} alt="p" />
+      <h1 className="titulo">Eleganza </h1>
+      <Formulario />
     </nav>
   );
-}
-
-
-
-
-
+};
--- src/date/api.js
+++
@@ -1,5 +0,0 @@
-export default async function api() {
-    const resp = await fetch("https://rickandmortyapi.com/api/character");
-    const products = await resp.json();
-    return products;
-}
